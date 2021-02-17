4a90ffe3e64bd93d3ef48081e0c0ff564366c6db
Chad <chadbsantiago@gmail.com>
2021-02-17 17:19:46 +0800

    updated name length validation

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flames import app
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- flames/forms.py
+++ flames/forms.py
@@ -1,5 +1,24 @@
 from wtforms import Form, StringField, validators
+from wtforms.validators import ValidationError
 
 class NameForm(Form):
-    firstname = StringField('Firstname', [validators.InputRequired(), validators.Length(min=3, max=60, message='name is too short!'), validators.Regexp(regex="^[a-zA-Z][\sa-zA-Z]*$", message=None)])
-    secondname = StringField('Secondname', [validators.InputRequired(), validators.Length(min=3, max=60, message='name is too short!'), validators.Regexp(regex="^[a-zA-Z][\sa-zA-Z]*$", message=None)])
+    firstname = StringField('Firstname', [validators.InputRequired(), validators.Regexp(regex="^[a-zA-Z][\sa-zA-Z]*$", message='Invalid character!')])
+    secondname = StringField('Secondname', [validators.InputRequired(), validators.Regexp(regex="^[a-zA-Z][\sa-zA-Z]*$", message='Invalid character!')])
+
+    def validate_firstname(self, firstname):
+        fname = firstname.data
+        fname = fname.replace(" ", "")
+
+        if len(fname) <= 2:
+            raise ValidationError('Name must contain at least 3 characters!')
+        elif len(fname) >= 20:
+            raise ValidationError('Name must not exceed 20 characters!')
+
+    def validate_secondname(self, secondname):
+        sname = secondname.data
+        sname = sname.replace(" ", "")
+
+        if len(sname) <= 2:
+            raise ValidationError('Name must contain at least 3 characters!')
+        elif len(sname) >= 20:
+            raise ValidationError('Name must not exceed 20 characters!')
